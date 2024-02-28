c057296f984177cda236248940a417fa20b3a7d8
Andres.Dev-PersonalNbk <esmicuentapersonal@gmail.com>
2024-02-27 22:49:47 -0300

    fix controller

--- src/main/java/ar/com/multiplecloudservices/controller/AfipController.java
+++ src/main/java/ar/com/multiplecloudservices/controller/AfipController.java
@@ -9,37 +9,40 @@ import ar.com.multiplecloudservices.model.PersonaTo;
 import ar.com.multiplecloudservices.service.AfipService;
 import lombok.AllArgsConstructor;
 
-
 @RestController
 @AllArgsConstructor
 public class AfipController {
 
 	private AfipService afipService;
 
-	@GetMapping(path = "/afip/{cuit}")
+	@GetMapping(path = "/byCuit/{cuit}")
 	public ResponseEntity<PersonaTo> afipData(@PathVariable String cuit) throws Exception {
-		PersonaTo persona= new PersonaTo();
-		if (!afipService.verificado(cuit)) {
+		PersonaTo persona = new PersonaTo();
+		if (!afipService.verificado(cuit))
 			throw new Exception("Cuit invalido. ");
-		}
+
 		try {
-			persona=afipService.getData(cuit);
+			persona = afipService.getData(cuit);
 		} catch (Exception e) {
 			throw new Exception("Error al obtener data. " + e.getMessage());
 		}
 
-		return ResponseEntity.ok(persona) ;
+		return ResponseEntity.ok(persona);
 	}
 
-	@GetMapping(path = "/cuit/{dni}")
-	public ResponseEntity<String> afipCuit(@PathVariable String dni) throws Exception {
-		String cuit="";
+	@GetMapping(path = "/byDni/{dni}")
+	public ResponseEntity<PersonaTo> afipCuit(@PathVariable String dni) throws Exception {
+		PersonaTo persona = new PersonaTo();
 		try {
-			cuit=afipService.getCuit(dni);
+			String cuit = afipService.getCuit(dni);
+			if (!afipService.verificado(cuit))
+				throw new Exception("Dni invalido. ");
+			persona = afipService.getData(cuit);
+
 		} catch (Exception e) {
 			throw new Exception("Error al obtener data. " + e.getMessage());
 		}
 
-		return ResponseEntity.ok(cuit) ;
+		return ResponseEntity.ok(persona);
 	}
 }
